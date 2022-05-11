5705fac38566ba55c2f46b4a380247649063fbc6
ahanafislam <ahanafislam@gmail.com>
2022-05-11 16:46:01 +0600

    Use JWT token

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
@@ -10,6 +11,20 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+      return res.status(401).send({ message: 'Unauthorized Access' });
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+      if (err) {
+          return res.status(403).send({ message: 'Forbidden Access' });
+      }
+      req.decoded = decoded;
+      next();
+  })
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.a00ff.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -19,12 +34,36 @@ async function run() {
     await client.connect();
     const inventoryItemsCollection = client.db("ashbab").collection("inventoryItems");
 
+    // Getnarate Auth token
+    app.post('/get_auth_token', async (req, res) => {
+      const user = req.body;
+      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+          expiresIn: '1d'
+      });
+      res.send({accessToken});
+    });
+
     // inventory items API
     app.get('/inventory', async (req, res) => {
         const query = {};
         const cursor = inventoryItemsCollection.find(query);
         const inventoryItems = await cursor.toArray();
         res.send(inventoryItems);
+    });
+
+    // GET User's item by auth token
+    app.get('/my_item', verifyJWT, async (req, res) => {
+      const decodedEmail = req.decoded.email;
+      const email = req.query.email;
+      if (email === decodedEmail) {
+        const query = {author_email: email};
+        const cursor = inventoryItemsCollection.find(query);
+        const inventoryItems = await cursor.toArray();
+        res.send(inventoryItems);
+      }
+      else {
+        res.status(403).send({message: "Forbidden Access"});
+      }
     });
 
     app.get('/inventory/:id', async (req, res) => {
